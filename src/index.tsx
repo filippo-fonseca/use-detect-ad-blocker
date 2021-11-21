d4cc18a3ecda52e12c990db581049083165c8380
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2021-11-21 17:54:14 -0600

    fix #1

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
+import React from "react";
+
 /**
  * A simple React hook that detects whether or not the user has enabled an ad blocker.
  * @return A boolean (whether or not an ad blocker is enabled).
  */
 export const useDetectAdBlocker = async () => {
-  let adBlockEnabled = false;
+  const [adBlockEnabled, setAdBlockEnabled] = React.useState(false);
   const googleAdUrl = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
   try {
-    await fetch(new Request(googleAdUrl)).catch((_) => (adBlockEnabled = true));
+    await fetch(new Request(googleAdUrl)).catch((_) => setAdBlockEnabled(false));
   } catch (e) {
-    adBlockEnabled = true;
+    setAdBlockEnabled(true);
   } finally {
     return adBlockEnabled;
   }
