44ccb6ea476e23f6a7ab0e44d072bfdb20d6cd15
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2021-11-21 15:33:38 -0600

    initial hook

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 /**
- * A highly complex function that adds two numbers together.
- * @param a The first number
- * @param b The second number
- * @return The two numbers added together
+ * A simple React hook that detects whether or not the user has enabled an ad blocker.
+ * @return A boolean (whether or not an ad blocker is enabled).
  */
-export function add(a: number, b: number) {
-  return a + b;
-}
+export const useDetectAdBlocker = async () => {
+  let adBlockEnabled = false;
+  const googleAdUrl = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+  try {
+    await fetch(new Request(googleAdUrl)).catch((_) => (adBlockEnabled = true));
+  } catch (e) {
+    adBlockEnabled = true;
+  } finally {
+    return adBlockEnabled;
+  }
+};
